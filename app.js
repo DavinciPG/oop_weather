5311ecd3c0f5781675f7edac2412c8da2d927741
Erki <erkiparna7@gmail.com>
2022-11-09 12:30:44 +0200

    Linna andmete muutmine

--- app.js
+++ app.js
@@ -2,6 +2,19 @@
 const weather = new Weather('Tallinn')
 const ui = new UI()
 
+// events
+const form = document.querySelector('#change-city')
+form.addEventListener('submit', changeWeather)
+
+// change weather
+function changeWeather(event) {
+    const city = document.querySelector('#city-name').value
+    weather.changeCity(city)
+    getWeather()
+    document.querySelector('#city-name').value = ''
+    event.preventDefault()
+}
+
 function getWeather() {
     weather.getWeather()
         .then(data => {
